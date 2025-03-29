c67081a678c1c548962b466e486c1abde18f974e
Muhammad Ridho <mail@muhammadridho.com>
2025-03-30 01:36:48 +0700

    commit

--- public/assets/js/webflow.js
+++ public/assets/js/webflow.js
@@ -1,12 +1,3 @@
-
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 ( () => {
     var e = {
         5487: function() {
